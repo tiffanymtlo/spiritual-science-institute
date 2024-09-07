f928cd15ab1695d0da4885aa7b258c1a8fd43082
tiffany lo <tiffany.mtlo@gmail.com>
2024-09-07 17:40:15 +0900

    updated Contact Us section and added materials UI

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
+import PhoneIcon from '@mui/icons-material/Phone';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import LaptopMacIcon from '@mui/icons-material/LaptopMac';
 import './ContactUs.css';
 
 const ContactUs = () => {
   return (
     <section className="contact-us">
       <h2>Contact Us</h2>
-      <div className="contact-details">
-        <p>Whatsapp: +852 9111 9111</p>
-        <p>info@mysite.com</p>
+      <hr className='contact-us-divider' />
+      <div className="contact-content">
+        <div className="contact-details">
+          <div>
+            <WhatsAppIcon />
+            <PhoneIcon />
+            <p> +852 6802 6168</p>
+          </div>
+          <div>
+            <MailOutlineIcon />
+            <p> pallasng@ichingspiritual.com</p>
+          </div>
+          <div>
+            <LaptopMacIcon />
+            <p> https://www.ichingspiritual.com</p>
+          </div>
+        </div>
         <p>Hong Kong</p>
       </div>
     </section>
